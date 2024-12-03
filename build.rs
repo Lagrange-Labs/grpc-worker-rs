71b6b92ca45b5b05e4e7133bb249720441d24473
nikkolasg <nikkolasg@gmail.com>
2024-12-03 18:34:22 +0100

    fixing path

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 use miette::IntoDiagnostic;
 
 fn main() -> miette::Result<()> {
-    let file_descriptors = protox::compile(["proto/lagrange.proto"], ["../lagrange-protobuf/"])?;
+    let file_descriptors = protox::compile(["proto/lagrange.proto"], ["lagrange-protobuf/"])?;
 
     // Bulid gRPC codegen
     println!("cargo:rerun-if-changed=../lagrange-protobuf/");
